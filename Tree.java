de13ccb1c8526f0fb74139ea1979f9e82ce7cbbd
DevHyeun <ykiw911@naver.com>
2020-01-13 16:06:06 +0900

    <수정> Tree.java : 트리구현

--- Tree.java
+++ Tree.java
@@ -9,10 +9,9 @@ public class Tree {
 		Node n5 = new Node("5");
 		
 		root.add(n2);
-        root.add(n3);
-        root.add(n4);
-        n2.add(n5);
-        root.print();
+		root.add(n3);
+		root.add(n4);
+		n3.add(n5);
 	}
 
 }
@@ -30,15 +29,15 @@ class Node{
 		this.children.add(child);
 	}
 	public void print() {
-		if(this.parent==null) {
+		if(this.parent==null) { /* 최상위 루트 노드일경우에만 본인 출력 */
 			System.out.println(this.data);
 		}
 		if(this.children.size()!=0) {
-			for(int i=0;i<this.children.size();i++) {
+			for(int i=0;i<this.children.size();i++) { /* 자식노드가 있을경우 모든 자식노드를 출력하고 */
 				System.out.print(this.children.get(i).data+" ");
 			}
 			System.out.println();
-			for(int i=0;i<this.children.size();i++) {
+			for(int i=0;i<this.children.size();i++) { /* 자식노드 마다 똑같이 재귀적으로 print함수 호출 */
 				this.children.get(i).print();
 			}
 		}
