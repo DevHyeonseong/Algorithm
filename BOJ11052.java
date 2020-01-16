7c646dc4d73afb54e6944099d57d98e985200b3b
DevHyeun <ykiw911@naver.com>
2020-01-16 17:43:30 +0900

    <추가> BOJ11052.java : 다이나믹프로그래밍

---
+++ BOJ11052.java
@@ -0,0 +1,32 @@
+/*
+ * BOJ11052
+ * 2020.01.16
+ * Hyeonseong
+ */
+import java.util.*;
+public class BOJ11052 {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+	}
+}
+class Solution{
+	public Solution() {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] p = new int[n+1];
+		int[] dp = new int[n+1];
+		
+		for(int i=1;i<n+1;i++) {
+			p[i] = scan.nextInt();
+		}
+		
+		dp[1] = p[1];
+		
+		for(int i=2 ;i<n+1;i++) {
+			for(int j=i;j>0;j--) {
+				dp[i] = Math.max(dp[i],dp[i-j]+p[j]);
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}
