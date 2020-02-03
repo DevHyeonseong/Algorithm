b4e3d363a219f1d4ff0e59d5e654b78421b8e00e
DevHyeun <ykiw911@naver.com>
2020-02-03 15:36:32 +0900

    <수정> BOJ11729.java : 분할정복

--- BOJ/BOJ11729.java
+++ BOJ/BOJ11729.java
@@ -4,22 +4,29 @@
  * Hyeonseong
  */
 import java.util.*;	
-public class BOJ11729 {
+public class BOJ1729 {
 	public static void main(String[] args) {
 		Solution s = new Solution();
 	}
 }
 class Solution{
 	int n;
+	int num;
+	StringBuilder sb;
 	public Solution() {
 		Scanner scan = new Scanner(System.in);
 		n = scan.nextInt();
+		sb = new StringBuilder();
+		num = 0;
 		go(n,1,3);
+		System.out.println(num);
+		System.out.print(sb.toString().trim());
 	}
 	public void go(int n, int x, int y) {
 		if(n==0) return;
 		go(n-1,x,6-x-y);
-		System.out.println(x+" "+y);
+		sb.append(x+" "+y+"\n");
+		num++;
 		go(n-1,6-x-y,y);
 	}
 }
